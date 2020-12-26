3e39d8671f6ba8b7c1ca4b1d4cb59804bcf6296e
MBS11 <70107735+MBS11@users.noreply.github.com>
2020-12-27 01:04:11 +0530

    first commit

--- iCoder/urls.py
+++ iCoder/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
+admin.site.site_header = "iBLOGr Admin"
+admin.site.site_title = "iBLOGr Admin Panel"
+admin.site.index_title = "Welcome to iBLOGr Admin Panel"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('blog/', include('blog.urls')),
 ]

2a95d260c8675b7165ea831be1b7b5b92660deed
MBS11 <70107735+MBS11@users.noreply.github.com>
2021-01-08 14:42:47 +0530

    F

--- home/models.py
+++ home/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Contact(models.Model):
@@ -11,3 +13,21 @@ class Contact(models.Model):
 
      def __str__(self):
          return "Message from " + self.name + ' - ' + self.email
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE,blank='true',null='true')
+    first_name= models.CharField(max_length=100,blank='true',null='true')
+    last_name= models.CharField(max_length=100,blank='true',null='true')
+    email= models.CharField(max_length=100,blank='true',null='true')
+
+    def __str__(self):
+         return str(self.user)
+    
+
+def create_Profile(sender, instance, created , **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        print("Profile created")
+
+post_save.connect(create_Profile, sender=User)
